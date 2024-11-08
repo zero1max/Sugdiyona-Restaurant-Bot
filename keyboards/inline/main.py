2b28d36452c1bc8f468ac521c8421905b735541a
Nigmatjonov Muhammadjon <148071379+zero1max@users.noreply.github.com>
2024-11-08 23:49:29 +0500

    Update Commit

--- keyboards/inline/main.py
+++ keyboards/inline/main.py
@@ -61,7 +61,6 @@ asosiymenu = InlineKeyboardMarkup(
 
 savat = InlineKeyboardMarkup(
     inline_keyboard=[
-        # [InlineKeyboardButton(text='⬅️', callback_data='previous'), InlineKeyboardButton(text='➡️', callback_data='next')],
         [InlineKeyboardButton(text="Savatga qo'shish📥", callback_data='savesavat')],
         [InlineKeyboardButton(text='Savat🛒', callback_data='savat')]
     ]
@@ -75,37 +74,31 @@ plusminus = InlineKeyboardMarkup(
 )
 
 def create_product_keyboard(products):
-    # InlineKeyboardButton ob'ektlarini saqlash uchun ro'yxat yaratamiz
     buttons = []
-    row = []  # Mahsulotlar tugmalarini juft bo'lib saqlash uchun vaqtinchalik ro'yxat
+    row = []  
 
     for index, product in enumerate(products):
-        # Har bir mahsulot uchun tugma yaratamiz
         button = InlineKeyboardButton(
             text=f"{product[1]}", 
             callback_data=f"product_{product[0]}"
         )
         row.append(button)
 
-        # Agar juft mahsulot bo'lsa yoki oxirgi elementga yetgan bo'lsak, qatorni `buttons` ga qo'shamiz
         if len(row) == 2 or index == len(products) - 1:
             buttons.append(row)
-            row = []  # Yangi qatorni yaratish uchun ro'yxatni tozalaymiz
+            row = []  
 
     if products:
         back_button = [InlineKeyboardButton(text='Orqaga qaytish', callback_data='back')]
         buttons.append(back_button)
 
-    # InlineKeyboardMarkup yaratamiz
     inline_kb = InlineKeyboardMarkup(inline_keyboard=buttons)
     return inline_kb
 
 def create_cart_button(product_id):
-    # Create InlineKeyboardButton with the correct callback_data
     button = InlineKeyboardButton(text="Savatchaga qo'shish", callback_data=f"savesavat_{product_id}")
     button1 = InlineKeyboardButton(text='Savat🛒', callback_data='savat')
     
-    # Create InlineKeyboardMarkup and add the button in a list
     keyboard = InlineKeyboardMarkup(inline_keyboard=[[button], [button1]])
     
     return keyboard
